359ff7e45de7cba7911c5685fe375e10c7b1e35c
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-08-30 16:15:03 +0300

    validator

---
+++ Service/ValidationScenarioService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rules_of_seo.Service
+{
+    public class ValidationScenarioService
+    {
+        public void Execute()
+        {
+
+        }
+    }
+}
