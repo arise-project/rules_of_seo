9cc47b878a954a4f252c5b1d61c2442bd36f12e4
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-08-30 14:15:08 +0300

    validator

---
+++ Service/Interface/IRuleValidatorService.cs
@@ -0,0 +1,14 @@
+﻿using rules_of_seo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rules_of_seo.Service
+{
+    public interface IRuleValidatorService
+    {
+        Dictionary<string, List<RuleMessage>> Validate(Dictionary<string, List<string>> content, Dictionary<string, Rule> rules);
+    }
+}
---
+++ Service/RuleValidatorService.cs
@@ -0,0 +1,17 @@
+﻿using rules_of_seo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rules_of_seo.Service
+{
+    public class RuleValidatorService : IRuleValidatorService
+    {
+        public Dictionary<string, List<RuleMessage>> Validate(Dictionary<string, List<string>> content, Dictionary<string, Rule> rules)
+        {
+            return null;
+        }
+    }
+}
