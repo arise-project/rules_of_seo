978b87bfabc7381b9c530fd2dc63dd9b29bf767c
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-08-30 13:38:28 +0300

    worker

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using rules_of_seo;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
