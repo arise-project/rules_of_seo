21082dafc0c5b61b1c2bd033c01b6c7f82a9cd9f
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-11 10:23:27 +0200

    draft

--- Validation/Rules/AllowAdditionParagraphValidator.cs
+++ Validation/Rules/AllowAdditionParagraphValidator.cs
@@ -7,14 +7,35 @@ namespace rules_of_seo.Validation.Rules
 {
     public class AllowAdditionParagraphValidator : IAllowAdditionParagraphValidator
     {
-        public AllowAdditionParagraphValidator()
-        {
-
-        }
+        public const string ParagraphTagBeg = "<p>";
+        public const string ParagraphTagBeg = "</p>";
 
         public RuleMessage Validate(PageChunk c, Rule r)
         {
-            return null;
+            int bc = 0, ec = 0, b, e;
+
+            do
+            {
+
+            }
+            while(b!= -1);
+
+            do
+            {
+
+            }
+            while(b!= -1);
+
+            if(bc!=ec)
+            {
+                
+            }
+
+            return new RuleMessage 
+            { 
+                MessageLevel = MessageLevel.Info,
+                
+            };
         }
     }
 }
